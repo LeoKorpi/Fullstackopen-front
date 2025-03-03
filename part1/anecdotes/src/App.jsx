b83b9724ea8444c8562b2f0fe3cefb7efbdaf82d
LeoKorpi <leokorpi.game@gmail.com>
2025-03-03 18:46:18 +0100

    lesson 1.13 done

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,13 +13,26 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
 
   return (
     <main>
+      <h1>Anecdote of the Day</h1>
       <p>{anecdotes[selected]}</p>
-      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
-        next anecdote
-      </button>
+      <p>Has {votes[selected]} votes</p>
+      <button onClick={() => handleVote()}>Vote</button>
+      <button onClick={() => handleNextAnecdote()}>Next Anecdote</button>
     </main>
   );
 };
