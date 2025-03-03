b7ceac0959113877097c5259c22303f9b4b904bc
LeoKorpi <leokorpi.game@gmail.com>
2025-03-03 17:30:23 +0100

    lesson 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positivePercetange = total === 0 ? 0 : (good / total) * 100;
 
   return (
     <main>
@@ -28,6 +31,9 @@ const App = () => {
         <p>good {good} </p>
         <p>neutral {neutral} </p>
         <p>bad {bad} </p>
+        <p>all {total}</p>
+        <p>average {average}</p>
+        <p>positive {positivePercetange}%</p>
       </section>
     </main>
   );
